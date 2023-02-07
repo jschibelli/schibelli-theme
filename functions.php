129277d7cf0de5e44a21cea3336671993780292c
John Schibelli <jschibelli@gmail.com>
2023-02-06 20:52:28 -0500

    Setting initial HTML
    Set base HTML including the main head files.

--- functions.php
+++ functions.php
@@ -1,6 +1,10 @@
 <?php
 function resume_files() {
-  wp_enqueue_style('resume_main_styles', get_stylesheet_uri());
+  wp_enqueue_script('main-theme-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+  wp_enqueue_style('custom-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('resume_main_styles', get_theme_file_uri('/build/style-index.css'));
+  wp_enqueue_style('resume_extra_styles', get_theme_file_uri('/build/index.css'));
 }
 
 add_action('wp_enqueue_scripts', 'resume_files');
